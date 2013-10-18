c0cf8878a32d3e06b01b5e624dc2746875ccd73d
Marius A. <marius.anderie@hotmail.com>
2013-10-18 23:38:53 +0200

    Update ascii_bin_conversion.c

--- ascii_bin_conversion.c
+++ ascii_bin_conversion.c
@@ -2,73 +2,124 @@
 #include <stdlib.h>
 #include <string.h>
 
-using namespace std;
 
+char* read_file_into_buffer (const char* filename, unsigned long* file_length);
 void ascii_to_bin(const char* ascii, const char* filename);
 void bin_to_ascii(const char* input_file, const char* output_file);
 
 int main(int argc, char** argv) {
-    
-    bin_to_ascii(argv[1], argv[1]);
-    
-    
+	
+	if (argc < 3) {
+		printf("usage:*.exe [input_file] [output_file]\n");
+		return 1;
+	}
+	
+	
+	//bin_to_ascii(argv[1], argv[2]);
+	
+	ascii_to_bin(read_file_into_buffer(argv[1], NULL), argv[2]);
+	
+
 }
 
 
 void bin_to_ascii(const char* input_file, const char* output_file) {
-    
-    /***   READ FILE INTO BUFFER ***/
-    FILE *file;
-    char *buffer;
-    unsigned long fileLen;
+	
+	unsigned long file_length[1];
+	char *buffer = read_file_into_buffer(input_file, file_length);
+	
 
-    //Open file
-    file = fopen(input_file, "rb");
-    if (!file) {
-        fprintf(stderr, "Can not open file %s", input_file);
-        return;
-    }
-    
-    fseek(file, 0, SEEK_END);
-    fileLen=ftell(file);
-    fseek(file, 0, SEEK_SET);
-    
-    buffer=(char *)malloc(fileLen+1);
-    
-    if (!buffer) {
-        fprintf(stderr, "Memory error!");
-        fclose(file);
-        return;
-    }
-    
-    fread(buffer, fileLen, 1, file);
-    fclose(file);
-    
-    
-    /*** CONVERT CHAR* TO HEX ASCII ***/
-    
-    for (unsigned long i = 0; i < fileLen; i++) {
-        
-        char hex[3];
-        unsigned char dec = buffer[i];
-        sprintf(hex, "%x", dec);
-        if (strlen(hex) < 2) {
-            hex[1] = hex[0];
-            hex[0] = '0';
-            hex[2] = '\0';
-        }
-        printf("%s   strlen: %d\n", hex, strlen(hex));
-    }
-    
-    
-    //Do what ever with buffer
-    free(buffer);
+	/*** CONVERT CHAR* TO HEX ASCII ***/
+	
+	
+	FILE* file;
+	file = fopen(output_file, "w");
+	
+	
+	if (!file) {
+		fprintf(stderr, "Can not create output file %s\n", output_file);
+		return;
+	}
+	
+	
+	for (unsigned long i = 0; i < *file_length; i++) {
+
+		char hex[3];
+		unsigned char dec = buffer[i];
+		sprintf(hex, "%x", dec);
+		if (strlen(hex) < 2) {
+			hex[1] = hex[0];
+			hex[0] = '0';
+			hex[2] = '\0';
+		}
+		fwrite(hex, strlen(hex), 1, file);
+		
+	}
+	
+	fclose(file);
+	
+	free(buffer);
+	
+}
+
+void ascii_to_bin(const char* ascii, const char* filename) {
+	
+	FILE* file;
+	file = fopen(filename, "wb");
+	if (!file) {
+		fprintf(stderr, "Can not create file %s\n", filename);
+		return;
+	}
+	
+	
+	for (unsigned long i = 0; i < strlen(ascii); i += 2) {
+		char hex[3];
+		unsigned char c;
+		hex[0] = ascii[i];
+		hex[1] = ascii[i+1];
+		hex[2] = '\0';
+		
+		sscanf(hex, "%x", &c);
+		fwrite(&c, sizeof(char), 1, file);
+		
+	}
+	
+	fclose(file);
+	
 }
 
 
+char* read_file_into_buffer (const char* filename, unsigned long* file_length) {
+	
+	/*** READ FILE INTO BUFFER ***/
+	FILE *file;
+	char *buffer;
+	unsigned long fileLen;
 
-void ascii_to_bin(const char* ascii, const char* filename) {
-    
-    
-    
+	//Open file
+	file = fopen(filename, "rb");
+	if (!file) {
+		fprintf(stderr, "Can not open file %s\n", filename);
+		return NULL;
+	}
+	
+	fseek(file, 0, SEEK_END);
+	fileLen=ftell(file);
+	if (file_length != NULL)
+		*file_length = fileLen;
+	
+	fseek(file, 0, SEEK_SET);
+
+	buffer=(char *)malloc(fileLen+1);
+
+	if (!buffer) {
+		fprintf(stderr, "Memory error!");
+		fclose(file);
+		return NULL;
+	}
+
+	fread(buffer, fileLen, 1, file);
+	fclose(file);
+	
+	return buffer;
 }
